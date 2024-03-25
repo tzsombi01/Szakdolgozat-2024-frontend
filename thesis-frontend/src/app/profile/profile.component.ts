b2c8d260e21cd2a47b0c70eeeed62538ec35e960
tzsombi01 <toreky.zsombor@gmail.com>
2024-03-25 21:02:51 +0100

    feature(profile-tickets): Profile and tickets component improvements

--- thesis-frontend/src/app/profile/profile.component.ts
+++ thesis-frontend/src/app/profile/profile.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  logout(): void {
+    console.log("SACASCSAFSA FGWDESAFW")
+  }
 
 }
--- thesis-frontend/src/app/ticket/ticket.component.ts
+++ thesis-frontend/src/app/ticket/ticket.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-ticket',
   templateUrl: './ticket.component.html',
   styleUrls: ['./ticket.component.css']
 })
-export class TicketComponent {
+export class TicketComponent implements OnInit {
 
   public tickets: any[] = [
     { ticketNumber: 1, assignee: 'John Doe', creator: 'Jane Smith' },
@@ -13,7 +13,15 @@ export class TicketComponent {
     { ticketNumber: 3, assignee: 'Charlie Parker', creator: 'David Wilson' }
   ];
 
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+    
+  }
+
   isTicketsEmpty(): boolean {
-    return false;
+    return this.tickets.length === 0;
   }
 }
